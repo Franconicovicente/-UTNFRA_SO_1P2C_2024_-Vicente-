0eb786d4027ba63ce1ab7c308f8bc20cc263a02f
Franconicovicente <franconicolassvicente@gmail.com>
2024-10-13 21:01:46 +0000

    Punto F terminado

---
+++ RTA_SCRIPT_Examen_20241011/Punto_F.sh
@@ -0,0 +1,9 @@
+Conocer mi IP publica usando
+curl -s ifconfig.me
+Conocer URL de mi repositorio usando:
+git remote get-url origin
+Conocer mi usuario:
+whoami
+Conocer el Hash de mi usuario:
+sudo grep vagrant /etc/shadow | awk -F ':' '{print}'
+Crear el archivo Filtro_avanzado.txt y copiar y pegar los datos
