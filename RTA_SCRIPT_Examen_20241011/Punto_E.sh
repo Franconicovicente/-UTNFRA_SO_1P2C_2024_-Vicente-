064cc7a35460cd5f3a563c3a8a18a22b6c275e66
Franconicovicente <franconicolassvicente@gmail.com>
2024-10-13 20:37:16 +0000

    Punto E terminado

---
+++ RTA_SCRIPT_Examen_20241011/Punto_E.sh
@@ -0,0 +1,6 @@
+Para saber el total de memoria RAM:
+sudo cat /proc/meminfo | grep MemTotal
+Para saber info del chasis:
+sudo dmidecode -t chassis
+cat repogit/UTNFRA_SO_1P2C_2024_-Vicente/RTA_ARCHIVOS_Examen_20241011/Filtro_Basico.txt 
+Copiar y pegar los datos obtenidos en ram y chasis
