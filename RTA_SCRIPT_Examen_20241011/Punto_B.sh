69ae81f4d4402921bd85247cdc0b3a6ac8aa0942
Franconicovicente <franconicolassvicente@gmail.com>
2024-10-13 18:33:52 +0000

    Punto A y B hechos

--- RTA_SCRIPT_Examen_20241011/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_B.sh
@@ -6,3 +6,6 @@ mkdir -p Examenes-UTN/alumno_{1..3}/parcial_{1..3}
 sudo mount /dev/sdc1 Examenes-UTN/alumno_1/parcial_1
 HASTA
 sudo mount /dev/sdc11 Examenes-UTN/profesores/
+blkid
+UUID="e004b8a4-cc30-421a-845a-5bd5f2ed9bcc" /Examenes-UTN/Alumno_1/parcial_1 ext4 defaults 0 0
+hasta el ultimo para montar de manera persistente
