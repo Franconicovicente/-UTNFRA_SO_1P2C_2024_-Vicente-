d628ebebf1965c7495a7ad09cfe282a3d36d4191
Franconicovicente <franconicolassvicente@gmail.com>
2024-10-17 15:59:31 +0000

    Todo scripteado

--- RTA_SCRIPT_Examen_20241011/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_C.sh
@@ -1,7 +1,10 @@
-sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$y$j9T$f7V.J8o8/5JXqaI3xUKKQ0$oND2iaEkOI22AcHc9kfKwylXqBi.VixFO/cPGuxMD.." p1c2_2024_A1
-Creacion de los demas usuarios...
 sudo group add p1c2_2024_gAlumno
 sudo groupadd p1c2_2024_gProfesores
+
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$y$j9T$f7V.J8o8/5JXqaI3xUKKQ0$oND2iaEkOI22AcHc9kfKwylXqBi.VixFO/cPGuxMD.." p1c2_2024_A1
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$y$j9T$f7V.J8o8/5JXqaI3xUKKQ0$oND2iaEkOI22AcHc9kfKwylXqBi.VixFO/cPGuxMD.." p1c2_2024_A2
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$y$j9T$f7V.J8o8/5JXqaI3xUKKQ0$oND2iaEkOI22AcHc9kfKwylXqBi.VixFO/cPGuxMD.." p1c2_2024_A3
+
 sudo chown p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/Alumno_1
 sudo chown p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/Alumno_2
 
